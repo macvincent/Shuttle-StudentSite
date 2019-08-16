6d9ce58a0eb7c750c47429708417408b77a88a48
MacVincent Agha-Oko <macvincentaghaoko@gmail.com>
2019-08-16 12:26:19 -0400

    fix: Restored to portable state

--- public/src.js
+++ public/src.js
@@ -1,105 +1,90 @@
-let map;
-
 async function getRoute(){
   let route = []
-  let populatedRoute = []
-  let url = "https://maps.googleapis.com/maps/api/directions/json?origin=34.682,-82.782&destination=35.227,-80.843&key=AIzaSyAgII8bDagXqXHG1te_iD-sS34vvamF4Dg";
   await $.ajax({
-      url: "http://127.0.0.1:8000/direction?url="+url, 
+      url: "http://127.0.0.1:8000/route/direction/7", 
       type: "GET",   
       dataType: 'json',
       cache: true,
       success: function(data){
-          data = data.routes[0].legs[0].steps;
-          data.forEach((entry) => {
-            route.push(entry.start_location);
-          });
-          for (let i = 1; i < route.length; i++){
-            let a = route[i-1];
-            let b = route[i];
-            //swap to a is always less than b
-            if(a.lat > b.lat){
-              let t = a;
-              a = b;
-              b = a;
-            }
-            //Make use of equation of a line to populate data set
-            let m = (b.lat - a.lat)/(b.lng - a.lng);
-            let c = a.lng - (m*a.lat);
-            let intv = 0.01;
-            while(a.lat < b.lat || a.lng < b.lng){
-              populatedRoute.push({lat: a.lat, lng: (m*a.lat) + c});
-              if(a.lat < b.lat){
-                a.lat += intv;
-              }else{
-                a.lng += 0.1;
-              }
-            }
-          }
+        route = JSON.parse(data);
       },
       error: function(data){
         console.log("Error: ", data);
       }           
   });
-  return populatedRoute;
+  return route;
 }
+var map;
 function initMap() {
-    let map = new google.maps.Map(document.getElementById('map'), {
+   map = new google.maps.Map(document.getElementById('map'), {
       zoom: 8,
-      center: {lat: 34.94957, lng: -81.93205},
+      center: {lat: 34.682, lng: -82.782},
       mapTypeId: 'terrain'
     });
-    getRoute().then(route => {
-      // Draw polyline
-      var flightPath = new google.maps.Polyline({
-        path: route,
-        geodesic: true,
-        strokeColor: '#FF0000',
-        strokeOpacity: 1.0,
-        strokeWeight: 2
-      });
-      flightPath.setMap(map);
-      //Set markers
-      let marker;
-      let userLocation;
-      var currpos = [];
-      setInterval(() => {
-        // Set position of user
-        if(userLocation)
-          userLocation.setMap(null)
+    let userLocation;
+    var currpos = [];
+    setInterval(() => {
+      if(userLocation)
+        userLocation.setMap(null)
 
-        if (navigator.geolocation) {
-          navigator.geolocation.watchPosition(response => {
-           currpos = response;
-          });
-        }
-        
-        if (currpos.coords){
-          userLocation = new google.maps.Marker({
-            position: new google.maps.LatLng(currpos.coords.latitude, currpos.coords.longitude),
-            map: map,
-            title: 'your current position'
-          });
-          userLocation.setMap(map);
-        }
-
-        // Get position of vehicle being tracked
-        $.get('http://127.0.0.1:8000/shuttle/4/').then(response => {
-          if(marker)
-            marker.setMap(null)
-          console.log("got here", response[0].longitude, response[0].latitude);
-          marker = new google.maps.Marker({
-            position: new google.maps.LatLng(response[0].latitude, response[0].longitude),
-            map: map,
-            icon: './car.png',
-            title:"Hello World!"
-          });
-          marker.setMap(map);
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(response => {
+        currpos = response;
         });
+      }
+      
+      if (currpos.coords){
+        userLocation = new google.maps.Marker({
+          position: new google.maps.LatLng(currpos.coords.latitude, currpos.coords.longitude),
+          map: map,
+          title: 'your current position'
+        });
+        userLocation.setMap(map);
+      }
+    }, 2000);
+} 
 
-      }, 1000);
+// Render route
+getRoute().then(flightPlanCoordinates => {
+  var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+  flightPath.setMap(map);
+  let marker;
 
-    }).catch(error => {
-      alert("sorry we have an error");
+  setInterval(() => {
+    // Set position of user
+
+    // // Get position of vehicle being tracked
+    // $.get('http://127.0.0.1:8000/shuttle/4/').then(response => {
+    //   if(marker)
+    //     marker.setMap(null)
+    //   console.log("got here", response[0].longitude, response[0].latitude);
+    //   marker = new google.maps.Marker({
+    //     position: new google.maps.LatLng(response[0].latitude, response[0].longitude),
+    //     map: map,
+    //     icon: './car.png',
+    //     title:"Hello World!"
+    //   });
+    //   marker.setMap(map);
+    // });
+
+  }, 1000);
+
+}).catch(error => {
+  alert("sorry we have an error");
+});
+
+function getRouteInfo(){
+  $.get("http://127.0.0.1:8000/route/getroutes/").then(data => {
+    data.forEach(route => {
+      $('ul').append(`<li class = "route_name"><a href="#">${route.route_name}</a></li>`)
     });
+  });
 }
+
+getRouteInfo();
