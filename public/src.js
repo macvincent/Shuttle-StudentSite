de88f8933461b5ebffce54edd78e8a8b0d9523fa
MacVincent Agha-Oko <macvincentaghaoko@gmail.com>
2019-08-25 15:25:37 -0400

    feat: Update user map view

--- public/src.js
+++ public/src.js
@@ -17,9 +17,10 @@ async function getRoute(id){
 var map;
 function initMap() {
    map = new google.maps.Map(document.getElementById('map'), {
-      zoom:10,
-      center: {lat: 34.682, lng: -82.782},
-      mapTypeId: 'terrain'
+      zoom:15,
+      center: {lat: 33.498070, lng: -80.854220},
+      mapTypeId: 'hybrid',
+      disableDefaultUI: true
     });
     let userLocation;
     var currpos = [];
@@ -30,7 +31,7 @@ function initMap() {
       if (navigator.geolocation) {
         navigator.geolocation.watchPosition(response => {
         currpos = response;
-        });
+        },(e) => console.log(e), {enableHighAccuracy: true});
       }
       
       if (currpos.coords){
@@ -39,6 +40,7 @@ function initMap() {
           map: map,
           title: 'your current position'
         });
+        map.setCenter({lat: currpos.coords.latitude, lng: currpos.coords.longitude});
         userLocation.setMap(map);
       }
     }, 2000);
@@ -63,7 +65,7 @@ function getRouteInfo(){
           geodesic: true,
           strokeColor: '#FF0000',
           strokeOpacity: 1.0,
-          strokeWeight: 2
+          strokeWeight: 4
         });
         flightPath.setMap(map);
         
